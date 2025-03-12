67268fbcf0b285e22e7b5b0129efc4373ec9ef0a
sofiebetzer <sofiebetzer14@gmail.com>
2025-03-12 10:15:14 +0100

    Character function

--- sketch.js
+++ sketch.js
@@ -1,14 +1,26 @@
+let characterX = 10
+let characterY = 480
+
 function setup() 
 {
-	createCanvas(600, 600);
+	createCanvas(1400, 600);
 }
 
 function draw()
 {
-	{
+
 background(135,206,235) 
 fill(255,176,39)
 ellipse(589, 10, 150, 150)
+character()
+
+}
+function character() {
+	fill(205,176,39)
+	rect(characterX, characterY, 60, 80)
 }
 
+function keyPressed() {
+let speed = 10
+if (key ) 
 }
