708c5d1f39ae5e60b77a04910afbad09c0a7f342
sofiebetzer <sofiebetzer14@gmail.com>
2025-03-20 15:00:58 +0100

    Min character ryger ikke ud fra canvas

--- sketch.js
+++ sketch.js
@@ -1,12 +1,14 @@
 let characterX = 50
-let characterY = 480
+let characterY = 515
 let speed = 5
 let hastighedY = 0
 let gravity = 0.2
 let isHop = false
 
+
+
 function setup() {
-	createCanvas(1400, 600);
+	createCanvas(1475, 600)
 }
 
 function draw(){
@@ -16,14 +18,13 @@ fill(255,176,39)
 ellipse(589, 10, 150, 150)
 
 characterY += hastighedY
-if (characterY < 480){
+if (characterY < 515){
 	hastighedY += gravity
 } else {
-	characterY = 480
+	characterY = 515
 	hastighedY = 0
 	isHop = false
 }
-
 
 
 keyPressed()
@@ -31,27 +32,34 @@ character()
 
 
 }
+
 function character() {
 	fill(205,176,39)
 	rect(characterX, characterY, 60, 80)
 }
 
 function keyPressed() {
+
 if (keyIsDown(32) && !isHop) {
 	hastighedY = -12
 	isHop = true
 }
 
-
-if (keyIsDown(65)){characterX -=speed}
-
-if (keyIsDown(68)){characterX +=speed}
-
+if (keyIsDown(65)){
+	if (characterX - speed >= 0) {
+		characterX -= speed
+	} else {
+		characterX = 0
+	}
 
 }
 
- 
+if (keyIsDown(68)){
+	if (characterX + speed <= width - 60){ 
+		characterX += speed
+	} else {
+		characterX = width - 60
+	}
+}
 
-
-
-
+}
