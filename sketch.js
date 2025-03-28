510a3dc4050c2165658d7fe52949abf862b1bb00
sofiebetzer <sofiebetzer14@gmail.com>
2025-03-28 14:54:52 +0100

    Coin collect

--- sketch.js
+++ sketch.js
@@ -4,8 +4,15 @@ let speed = 5
 let hastighedY = 0
 let gravity = 0.2
 let isHop = false
-
-
+let coinCollected = false
+let coins = [
+	{x:250, y:400, collected: false},
+	{x:320, y:400, collected: false},
+	{x:600, y:300, collected: false},
+	{x:900, y:515, collected: false},
+	{x:1200, y:450, collected: false}	
+]
+let score = 0
 
 function setup() {
 	createCanvas(1475, 600)
@@ -14,6 +21,9 @@ function setup() {
 function draw(){
 background(135,206,235) 
 
+fill(0)
+textSize(24)
+text("score: " + score,20,40)
 
 fill(255,176,39)
 ellipse(589, 10, 150, 150)
@@ -27,20 +37,31 @@ if (characterY < 515){
 	isHop = false
 }
 
+for (let i = 0; i < coins.length; i++){
+	let c = coins[i]
+	if (!c.collected){
+		if(
+			characterX < c.x + 20 &&
+			characterX + 60 > c.x &&
+			characterY < c.y + 20 &&
+			characterY + 70 > c.y
+		){
+			c.collected = true
+			score += 1
+		}
+	}
+}
 
+
+platform()
 keyPressed()
 character()
-platform()
+coin()
 
 }
 
 function platform(){
-  let leftWall = 200;
-  let rightWall = 300
-  line(leftWall, 10, leftWall, 500);
-  line(rightWall, 10, rightWall, 500)
-	
-  let character= constrain(characterX,leftWall,rightWall)
+	rect(200,450,400,20)
 }
 
 function character() {
@@ -48,8 +69,17 @@ function character() {
 	rect(characterX, characterY, 60, 70)
 }
 
-function keyPressed() {
+function coin(){
+	for (let i = 0; i < coins.length; i++){
+		let c = coins[i]
+		if(!c.collected){
+			fill(255,223,0)
+			ellipse(c.x, c.y, 20, 20)
+		}
+	}
+}
 
+function keyPressed() {
 if (keyIsDown(32) && !isHop) {
 	hastighedY = -12
 	isHop = true
