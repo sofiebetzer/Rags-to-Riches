6b840fecfeea7f5ce60a3531ffac13ce4450a23a
sofiebetzer <sofiebetzer14@gmail.com>
2025-04-20 16:23:33 +0200

    Trying

---
+++ sketchv2.js
@@ -0,0 +1,167 @@
+let fattig
+let flip
+let city
+let money
+let bjerg
+let characterX = 10
+let characterY = 515
+let speed = 5
+let hastighedY = 0
+let gravity = 0.30
+let isHop = false
+let hop = 0
+let gameState = "start"
+let coinCollected = false
+let score = 0
+let coins = [
+	{x:250, y:400, collected: false},
+	{x:320, y:400, collected: false},
+	{x:600, y:300, collected: false},
+	{x:900, y:215, collected: false},
+	{x:1200, y:450, collected: false},
+	{x:400, y:380, collected: false},	
+]
+
+function preload(){
+	fattig = loadImage("Fattig1.png")
+	city = loadImage("BY.jpg")
+	flip = loadImage("Fattig-flip.PNG")
+	money = loadImage("Coin.png")
+	bjerg = loadImage("Mountain.jpg")
+}
+
+function setup() {
+	createCanvas(1475, 600)
+}
+
+function draw(){
+if (gameState === "start"){
+	startScreen()
+} else if (gameState === "play"){
+	playGame()
+}
+}
+
+function startScreen(){
+	image(bjerg,0,0,1475,600)
+	textSize(50)
+	textAlign(CENTER)
+	text("Welcome to Rags to Riches", width/2, height/2 - 50)
+	textSize(24)
+	text("Press ENTER to start the game", width/2, height/2 + 50)
+}
+
+function playGame(){
+image(city,0,0,1475,600) 
+
+fill(255,255,255)
+textSize(24)
+text("score: " + score,20,40)
+
+
+
+
+characterY += hastighedY
+hop += 0.2
+
+if (characterY < 515){
+	hastighedY += gravity
+} else {
+	characterY = 515
+	hastighedY = 0
+	isHop = false
+}
+
+
+for (let i = 0; i < coins.length; i++){
+	let c = coins[i]
+	if (!c.collected){
+		if(
+			characterX < c.x + 20 &&
+			characterX + 60 > c.x &&
+			characterY < c.y + 20 &&
+			characterY + 70 > c.y
+		){
+			c.collected = true
+			score += 1
+		}
+	}
+}
+fill(150,75,0)
+rect(200,450,400,20)
+rect(800,350,400,20)
+
+character()
+AD()
+coin()
+
+
+
+if(characterX + 60 > 200 && characterX < 560
+	&& characterY + 60 > 450 && characterY + 60 < 470 &&
+	hastighedY >= 0
+){
+	characterY = 450 - 60
+	hastighedY = 0
+	isHop = false
+	}
+
+if(characterX + 60 > 800 && characterX < 1170
+	&& characterY + 60 > 350 && characterY + 60 < 370 &&
+hastighedY >= 0){
+	characterY = 350 - 60
+	hastighedY = 0
+	isHop = false
+}
+
+}
+
+function character() {
+	if (keyIsDown(68)){
+	image(fattig, characterX, characterY, 90, 60)} 
+	else if (keyIsDown(65)){ image(flip,characterX,characterY,90,60)
+	} else { image(fattig, characterX, characterY, 90, 60)
+	}
+}
+
+function coin(){
+	for (let i = 0; i < coins.length; i++){
+		let c = coins[i]
+		if(!c.collected){
+			image(money,c.x, c.y, 20, 20)
+		}
+	}
+}
+
+function AD(){
+	if (keyIsDown(65)){
+		if (characterX - speed >= 0) {
+			characterX -= speed
+		} else {
+			characterX = 0
+		}
+	
+	}
+	
+	if (keyIsDown(68)){
+		if (characterX + speed <= width - 60){ 
+			characterX += speed
+		} else {
+			characterX = width - 60
+		}
+	}
+}
+
+
+function keyPressed() {
+	if (gameState === "start" && keyCode === 13 ){
+		gameState = "play"
+	}
+	if (gameState === "play"){
+		if (keyCode === 32 && !isHop){
+			hastighedY = -10
+			hop = -1
+			isHop = true
+		}
+	}
+}
